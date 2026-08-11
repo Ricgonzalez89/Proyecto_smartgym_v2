a11e71048831a6f7b64a0036c9e6db77763adb80
Ricgonzalez89 <ricardoa.gonzalez04@gmail.com>
2026-08-10 21:26:17 -0400

    Adicion de constantes para el sistema

---
+++ backend/app/core/constants.py
@@ -0,0 +1,63 @@
+from enum import Enum
+
+
+class Rol_Enum(str, Enum):
+    """
+    Enumeracion para estandarizar los roles existentes en el sistema.
+    """
+
+    ADMINISTRACION = "Administración"
+    FINANZAS = "Finanzas"
+    ENTRENADORES = "Entrenadores"
+    CLIENTES = "Clientes"
+
+
+class Estado_Oper_Maquina_Enum(str, Enum):
+    """
+    Estados operativos para las maquinas (usado tambien dentro de los tickets de
+    mantenimiento).
+    """
+
+    ACTIVA = "Activa"
+    MANTENIMIENTO = "En mantenimiento"
+    FUERA_SERVICIO = "Fuera de servicio"
+
+
+class Status_Reserva_Enum(str, Enum):
+    """
+    Estatus validos para las reservas de sesiones deportivas.
+    """
+
+    PENDIENTE = "Pendiente"
+    ASISTENTE = "Asistente"
+    NO_ASISTENTE = "No asistente"
+    CANCELADA = "Cancelada"
+
+
+class Status_Sesion_Enum(str, Enum):
+    """
+    Estatus validos para una sesion deportiva.
+    """
+
+    PROGRAMADA = "Programada"
+    FINALIZADA = "Finalizada"
+    CANCELADA = "Cancelada"
+
+
+class Actividad_Membresia_Enum(str, Enum):
+    """
+    Estados de actividad posibles para una membresia de un cliente.
+    """
+
+    ACTIVA = "Activa"
+    VENCIDA = "Vencida"
+    POR_VENCER = "Por vencer"
+
+
+class Descripcion_Pago_Enum(str, Enum):
+    """
+    Descripciones posibles de los pagos realizados por un cliente para un plan.
+    """
+
+    ADQUISICION = "Adquision de plan"
+    RENOVACION = "Renovacion de plan"
